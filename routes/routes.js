425cc081673b0d3fc09855afccdf1c94a1815a2a
Mai Van Linh <linh202987@gmail.com>
2023-06-20 23:32:11 +0700

    update

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const Product = require('../models/products');
+const multer = require('multer');
+
+var storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, './uploads');
+    },
+    filename: function(req, file, cb){
+        cb(null, file.fieldname + "_" + Date.now() + "_" +file.originalname);
+    },
+});
+
+var uploads = multer({
+    storage: storage,
+}).single("image");
 
 router.get("/", (req, res) => {
     res.render("index", { title: "Home page "});
